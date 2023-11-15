ef68672332d2cf627f7a662304dc8011d41dada3
Miguel Angel Islas Mendez <islasmiguel737@gmail.com>
2023-11-14 20:23:41 -0600

    13 Correcion de errores

--- src/MatrizTriangularSuperiorYInferior.java
+++ src/MatrizTriangularSuperiorYInferior.java
@@ -9,25 +9,27 @@ public class MatrizTriangularSuperiorYInferior{
 
     public static int[][] leerMatriz(int filas, int columnas, char nombre) throws IOException {
         int[][] matriz = new int[filas][columnas];
-
-        for(int i = 0; i < filas; i++){
-            for(int j = 0; i < columnas; j++){
-                            System.out.println("Escribe el valor de" + nombre + "[" + i +"][" + j +"]: ");
-            entrada = buffer.readLine();
-            matriz[i][j] = Integer.parseInt(entrada);
+    
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                System.out.println("Escribe el valor de " + nombre + "[" + i + "][" + j + "]: ");
+                entrada = buffer.readLine();
+                matriz[i][j] = Integer.parseInt(entrada);
             }
-        }
+        }        
         return matriz;
     }
+    
 
-    public static void imprimirMatriz(int[][] matriz, char nombre){
-        for(int i = 0; i < matriz.length; i++){
-            for( int j = 0; j < matriz[0].length; j++){
+    public static void imprimirMatriz(int[][] matriz, char nombre) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
                 System.out.print(matriz[i][j] + "  ");
             }
-            System.out.println(entrada);
+            System.out.println(); 
         }
     }
+    
 
     public static boolean esTriangularSuperior(int[][] matriz){
         for(int i = 0; i < matriz.length; i++){
@@ -53,35 +55,34 @@ public class MatrizTriangularSuperiorYInferior{
 
     public static void main(String[] args) throws Exception {
         int filas, columnas;
-
-        System.out.println("Que tipo de matriz quieres usas? (superior/inferior):");
+    
+        System.out.println("Que tipo de matriz quieres usar? (superior/inferior):");
         entrada = buffer.readLine();
         String tipoMatriz = entrada.toLowerCase();
-
-        System.out.println("Ingresar el nuemro de filas:");
+    
+        System.out.println("Ingresar el número de filas:");
         entrada = buffer.readLine();
         filas = Integer.parseInt(entrada);
-
-        System.out.println("Ingresar el numero de columnas:");
+    
+        System.out.println("Ingresar el número de columnas:");
         entrada = buffer.readLine();
         columnas = Integer.parseInt(entrada);
-
+    
         int[][] A = leerMatriz(filas, columnas, 'A');
-
-        System.out.println("Las Matrices");
+    
+        System.out.println("La Matriz:");
         imprimirMatriz(A, 'A');
-
+    
         boolean esTriangularSuperior = esTriangularSuperior(A);
         boolean esTriangularInferior = esTriangularInferior(A);
-
-        if("MatrizSuperior" == tipoMatriz && esTriangularSuperior){
+    
+        if ("superior".equals(tipoMatriz) && esTriangularSuperior) {
             System.out.println("La matriz es Triangular Superior");
-        } else if 
-            ("MatrizInferior" == tipoMatriz && esTriangularInferior){
-                System.out.println("La matriz es Triangular Inferior");
-            } else{
-                System.out.println("La matriz no es correcta");
-            }
+        } else if ("inferior".equals(tipoMatriz) && esTriangularInferior) {
+            System.out.println("La matriz es Triangular Inferior");
+        } else {
+            System.out.println("La matriz no es del tipo solicitado");
         }
+    }
     }
 
