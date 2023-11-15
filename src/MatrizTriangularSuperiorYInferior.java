f88eacb19ba26c10d151cb5df6105d1b0c5c9a6d
Miguel Angel Islas Mendez <islasmiguel737@gmail.com>
2023-11-14 19:31:51 -0600

    02 Contruyendo la lectura d ela matriz

--- src/MatrizTriangularSuperiorYInferior.java
+++ src/MatrizTriangularSuperiorYInferior.java
@@ -1,5 +1,23 @@
-public class App {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MatrizTriangularSuperiorYInferior{
+
+    public static BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+    public static String entrada;
+
+    public static int[][] leerMatriz(int filas, int columnas, char nombre) throws IOException {
+        int[][] matriz = new int[filas][columnas];
+
+        for(int i = 0; i < filas; i++){
+            System.out.println("Escribe el valor de" + nombre + "[" + i +"][" + j +"]: ");
+            entrada = buffer.readLine();
+            matriz[i][j] = Integer.parseInt(entrada);
+        }
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
     }
 }
