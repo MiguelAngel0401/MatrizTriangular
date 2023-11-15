bac3c5c9ea1bfedf2ef6e186929d5a36bf7410e8
Miguel Angel Islas Mendez <islasmiguel737@gmail.com>
2023-11-14 19:44:27 -0600

    06 Correcion por que me marcaba error faltaba un return

--- src/MatrizTriangularSuperiorYInferior.java
+++ src/MatrizTriangularSuperiorYInferior.java
@@ -37,6 +37,7 @@ public class MatrizTriangularSuperiorYInferior{
                 }
             }
         }
+        return true;
     }
 
     public static void main(String[] args) throws Exception {
